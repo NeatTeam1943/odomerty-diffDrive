07c70eb9395319e922aee6a200e50af7a93ae5c2
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-04 15:04:01 +0300

    Add driving functionality

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,20 +1,25 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.fasterxml.jackson.databind.util.PrimitiveArrayBuilder;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrain extends SubsystemBase {
@@ -25,6 +30,9 @@ public class DriveTrain extends SubsystemBase {
 
   private final MutableMeasure<Distance> m_leftPosition;
   private final MutableMeasure<Distance> m_rightPosition;
+
+  private final MutableMeasure<Velocity<Distance>> m_leftVelocity;
+  private final MutableMeasure<Velocity<Distance>> m_rightVelocity;
 
   private final Pigeon2 m_imu;
 
@@ -51,9 +59,14 @@ public class DriveTrain extends SubsystemBase {
     m_leftPosition = MutableMeasure.zero(Units.Meters);
     m_rightPosition = MutableMeasure.zero(Units.Meters);
 
+    m_leftVelocity = MutableMeasure.zero(Units.MetersPerSecond);
+    m_rightVelocity = MutableMeasure.zero(Units.MetersPerSecond);
+
     m_kinematics = new DifferentialDriveKinematics(DriveTrainConstants.kTrackWidth);
 
     setupMotorFollowrs();
+
+    m_odometry = new DifferentialDriveOdometry(m_imu.getRotation2d(), m_rightPosition, m_leftPosition);
   }
 
   private void setupMotorFollowrs() {
@@ -63,24 +76,67 @@ public class DriveTrain extends SubsystemBase {
     m_rightFollower.setInverted(true);
   }
 
-  private void updateEncoderPosition() {
+  private void updateEncoders() {
     double leftPosAvg = (m_leftMaster.getPosition().getValueAsDouble() + m_leftFollower.getPosition().getValueAsDouble()) / 2;
     double rightPosAvg = (m_rightMaster.getPosition().getValueAsDouble() + m_rightFollower.getPosition().getValueAsDouble()) / 2;
 
-    m_leftPosition.mut_replace(Units.Meter.of(nativeUnitsToDistanceMeters(leftPosAvg))); 
-    m_rightPosition.mut_replace(Units.Meter.of(nativeUnitsToDistanceMeters(rightPosAvg)));
+    double leftPosVelocity = (m_leftMaster.getVelocity().getValueAsDouble() + m_leftFollower.getVelocity().getValueAsDouble()) / 2;
+    double rightPosVelocity = (m_rightMaster.getVelocity().getValueAsDouble() + m_rightFollower.getVelocity().getValueAsDouble()) / 2;
+
+    rotationsToDistanceMeters(leftPosAvg, rightPosAvg, leftPosVelocity, rightPosVelocity);
   }
 
-  private double nativeUnitsToDistanceMeters(double sensorCounts){
+  public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+    final double leftFF = m_feedforward.calculate(speeds.leftMetersPerSecond);
+    final double rightFF = m_feedforward.calculate(speeds.rightMetersPerSecond);
+
+    final double leftOutput = m_leftPIDController.calculate(m_leftVelocity.magnitude(), speeds.leftMetersPerSecond);
+    final double rightOutput = m_rightPIDController.calculate(m_rightVelocity.magnitude(), speeds.rightMetersPerSecond);
+
+    m_leftMaster.setVoltage(leftOutput + leftFF);
+    m_rightMaster.setVoltage(rightOutput + rightFF);
+  }
+
+  public void drive(Measure<Velocity<Distance>> xSpeed, Measure<Velocity<Angle>> rot) {
+    var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, Units.MetersPerSecond.zero(), rot));
+    setSpeeds(wheelSpeeds);
+  }
+
+  private double nativeUnitsToWheelRotations(double sensorCounts){
     double motorRotations = (double)sensorCounts / DriveTrainConstants.kFalconCPR;
     double wheelRotations = motorRotations / DriveTrainConstants.kChassisGR;
-    var position = DriveTrainConstants.kWheelDiameter.times(Math.PI).times(wheelRotations);
 
-    return position.in(Units.Meters);
+    return wheelRotations;
+  }
+
+  private void rotationsToDistanceMeters(double leftPosAvg, double rightPosAvg, double leftPoseVelocity, double rightPosVelocity) {
+    var leftPosition = Units.Meters.of(nativeUnitsToWheelRotations(leftPosAvg)).times(Math.PI).times(DriveTrainConstants.kWheelDiameter);
+    var rightPosition = Units.Meters.of(nativeUnitsToWheelRotations(rightPosAvg)).times(Math.PI).times(DriveTrainConstants.kWheelDiameter);
+
+    var leftVelocity = Units.MetersPerSecond.of(nativeUnitsToWheelRotations(leftPoseVelocity)).times(Math.PI).times(DriveTrainConstants.kWheelDiameter);
+    var rightVelocity = Units.MetersPerSecond.of(nativeUnitsToWheelRotations(rightPosVelocity)).times(Math.PI).times(DriveTrainConstants.kWheelDiameter);
+
+    m_leftPosition.mut_replace(leftPosition.magnitude(), Units.Meters);
+    m_rightPosition.mut_replace(rightPosition.magnitude(), Units.Meters);
+    m_leftVelocity.mut_replace(leftVelocity.magnitude(), Units.MetersPerSecond);
+    m_rightVelocity.mut_replace(rightVelocity.magnitude(), Units.MetersPerSecond);
+  }
+
+  public void updateOdometry() {
+    m_odometry.update(m_imu.getRotation2d(), m_rightPosition.magnitude(), m_leftPosition.magnitude());
+  }
+
+  public Command drive(CommandXboxController joystick) {
+    return new RunCommand(() -> {
+      final var xSpeed = DriveTrainConstants.kDriveSpeed.times(-joystick.getLeftY());
+      final var rot = DriveTrainConstants.kTurnSpeed.times(-joystick.getRightX());
+      drive(xSpeed, rot);
+    }, this);
   }
 
   @Override
   public void periodic() {
-    updateEncoderPosition();
+    updateEncoders();
+    updateOdometry();
   }
 }
