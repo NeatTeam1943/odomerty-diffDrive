9e4d3f055ffcf0d2384007b03354b9762602444b
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-04 15:04:42 +0300

    Utilise DriveTrain in robotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,24 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DriveTrain;
 
 
 public class RobotContainer {
+  private final DriveTrain m_drive;
+  private final CommandXboxController m_joystick;
+  
   public RobotContainer() {
+    m_joystick = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    m_drive = new DriveTrain();
+   
+    m_drive.setDefaultCommand(m_drive.drive(m_joystick));
     configureBindings();
   }
 
-  private void configureBindings() {
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return null;
